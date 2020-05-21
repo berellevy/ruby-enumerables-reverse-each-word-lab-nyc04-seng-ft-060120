d51fbe24d997b9a69837d8d5f594c348eaf9a456
Berel Levy <berellevy@gmail.com>
2020-05-21 20:31:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,9 +3,9 @@ require "pry"
 def reverse_each_word(sentence)
   sentence = sentence.split(" ")
   # binding.pry
-  sentence.collect |word|
+  sentence.collect { |word|
     word.reverse()
     binding.pry
-  end
+  }
   sentence.join(" ")
 end
